802f03a90326de18c48ca4c8dfb7be4a2b1b4ea3
Roland <57312653+Landon-12@users.noreply.github.com>
2020-12-16 20:55:37 +0400

    TravelTure

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.prices__slider').slick({
+        prevArrow: '<button type="button" class="slick-arrow slick-next"><img class="arrow-icon" src="images/prices/prev-icon.svg" alt="next"></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-prev"><img class="arrow-icon" src="images/prices/next-icon.svg" alt="prev"></button>',
+    });
+});
